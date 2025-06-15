375404fcc3bee1735e6a8baa8b4b40be3932c0f6
vhoangcnr <phantriviethoang@gmail.com>
2025-06-15 16:15:35 +0700

    Refactor: Move Contact component, use props, remove hardcoded data

--- learn-react/App.jsx
+++ learn-react/App.jsx
@@ -1,9 +1,4 @@
-import Contact from "./components/Contact"
-
-/**
- * Challenge (I'm sorry!): Add all the rest of the
- * data to the contact card instances. 😈
- */
+import Contact from "./Contact"
 
 function App() {
     return (
@@ -26,7 +21,7 @@ function App() {
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
             />
-            <Contact
+            <Contact 
                 img="./images/pumpkin.png"
                 name="Pumpkin"
                 phone="(0800) CAT KING"
@@ -37,95 +32,3 @@ function App() {
 }
 
 export default App
-
-
-/**
- * 
-<article className="contact-card">
-    <img 
-        src="./images/mr-whiskerson.png"
-        alt="Photo of Mr. Whiskerson"
-    />
-    <h3>Mr. Whiskerson</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-1234</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>mr.whiskaz@catnap.meow</p>
-    </div>
-</article>
-
-<article className="contact-card">
-    <img 
-        src="./images/fluffykins.png"
-        alt="Photo of Fluffykins"
-    />
-    <h3>Fluffykins</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-2345</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>fluff@me.com</p>
-    </div>
-</article>
-
-<article className="contact-card">
-    <img 
-        src="./images/felix.png"
-        alt="Photo of Felix"
-    />
-    <h3>Felix</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(212) 555-4567</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>thecat@hotmail.com</p>
-    </div>
-</article>
-
-<article className="contact-card">
-    <img 
-        src="./images/pumpkin.png"
-        alt="Photo of Pumpkin"
-    />
-    <h3>Pumpkin</h3>
-    <div className="info-group">
-        <img 
-            src="./images/phone-icon.png" 
-            alt="phone icon" 
-        />
-        <p>(0800) CAT KING</p>
-    </div>
-    <div className="info-group">
-        <img 
-            src="./images/mail-icon.png" 
-            alt="mail icon"
-        />
-        <p>pumpkin@scrimba.com</p>
-    </div>
-</article>
- */
---
+++ learn-react/Contact.jsx
@@ -0,0 +1,33 @@
+/**
+ * Challenge: Fix the code below to use the `props`
+ * object values in place of the hardcoded values below
+ * 
+ * Note: There will be a small bug in the code, so do your
+ * best to squash it! 🐛
+ */
+
+export default function Contact(props) {
+    return (
+        <article className="contact-card">
+            <img
+                src={props.img}
+                alt="Photo of Mr. Whiskerson"
+            />
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img
+                    src="./images/phone-icon.png"
+                    alt="phone icon"
+                />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img
+                    src="./images/mail-icon.png"
+                    alt="mail icon"
+                />
+                <p>{props.email}</p>
+            </div>
+        </article>
+    )
+}
