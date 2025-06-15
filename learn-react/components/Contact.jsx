e1cd7b7428dbdb4e93706f6780f7a635d7db3223
vhoangcnr <phantriviethoang@gmail.com>
2025-06-15 15:56:38 +0700

    feat: Initialize React project

---
+++ learn-react/components/Contact.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+    return (
+        <div className="contacts">
+            <article className="contact-card">
+                <img
+                    src="./images/mr-whiskerson.png"
+                    alt="Photo of Mr. Whiskerson"
+                />
+                <h3>Mr. Whiskerson</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" alt="phone icon" />
+                    <p>(212) 555-1234</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" alt="mail icon" />
+                    <p>mr.whiskaz@catnap.meow</p>
+                </div>
+            </article>
+        </div>
+    );
+}
